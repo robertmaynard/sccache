cbdd78964b92d41352732b34678ba4e953a17e70
Robert Maynard <robert.maynard@kitware.com>
2020-04-03 16:08:41 -0400

    Add ptx compilation support to nvcc

--- src/compiler/nvcc.rs
+++ src/compiler/nvcc.rs
@@ -160,6 +160,7 @@ counted_array!(pub static ARGS: [ArgInfo<gcc::ArgData>; _] = [
     flag!("-dc", DoCompilation),
     take_arg!("-gencode", OsString, CanBeSeparated('='), PassThrough),
     take_arg!("-maxrregcount", OsString, CanBeSeparated('='), PassThrough),
+    flag!("-ptx", DoCompilation),
     take_arg!("-rdc", OsString, CanBeSeparated('='), PreprocessorArgument),
     take_arg!("-x", OsString, CanBeSeparated('='), Language),
 
